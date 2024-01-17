2fbca38e3e85d1d2517b8a942540d51d75429258
T.Abramczyk1126 <t.abramczyk@gmail.com>
2024-01-17 23:38:50 +0100

    Final version of Calc

--- src/main/java/org/age/Adult.java
+++
@@ -1,5 +0,0 @@
-package org.age;
-
-public static boolean isAdult(int age) {
-    return age >= 18;
-}
---
+++ src/main/java/org/age/Age.java
@@ -0,0 +1,11 @@
+package org.age;
+
+public class Age {
+
+    public static boolean isAdult(int age){
+        if (age >= 18){
+            return true;
+        }
+        return false;
+    }
+}
