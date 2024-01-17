df217d475d5e0d1423c29c2f0b1aa2c6cd2c0ffe
T.Abramczyk1126 <t.abramczyk@gmail.com>
2024-01-17 19:05:35 +0100

    First project maven

---
+++ src/test/java/org/calculator/Task2_ProjectMavenTest.java
@@ -0,0 +1,12 @@
+package org.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Task2_ProjectMavenTest {
+
+    @Test
+    void main() {
+    }
+}
